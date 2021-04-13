de5c2f02c898c4cfcafe50ae78abd0741e9f9823
Sarah Smith <sarah@smithsoft.com.au>
2021-04-13 17:09:32 +1000

    Remove generated files

--- server/dist/js/controllers/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const signup = (req, res) => {
-    res.json({
-        data: 'you hit signup endpoint blah'
-    });
-};
-exports.default = signup;
