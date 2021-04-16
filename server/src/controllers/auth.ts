8c874730005a02219a70fc665af03694a07f94f3
Sarah Smith <sarah@smithsoft.com.au>
2021-04-16 16:38:07 +1000

    Fix email auth

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,6 +1,10 @@
 import { Response, Request } from "express"
 import User from "../models/user"
+import sendmail from "../helpers/sendmail"
+import * as jwt from "jsonwebtoken"
+import { Credentials } from "./../types/credentials"
 
+/**
 const signup = (req: Request, res: Response) => {
     const { name, email, password } = req.body
 
@@ -31,5 +35,84 @@ const signup = (req: Request, res: Response) => {
         }
     )
 }
+ * 
+ * @param req 
+ * @param res 
+ */
 
-export default signup
+const signUp = (req: Request, res: Response) => {
+    const { name, email, password } = req.body
+
+    User.findOne({ email }).exec(
+        (err, user) => {
+            if (user) {
+                return res.status(400).json({
+                    error: 'Email is taken'
+                })
+            }
+        }
+    )
+    const account_activation: string = process.env.JWT_ACCOUNT_ACTIVATION!
+    const email_account = process.env.EMAIL_USER
+    const email_password = process.env.EMAIL_PASS
+    const client_url = process.env.CLIENT_URL
+
+    const token = jwt.sign(
+        { name, email, password },
+        account_activation,
+        { expiresIn: "10m" }
+    )
+
+    const emailData = {
+        from: email_account, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+        to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+        subject: "ACCOUNT ACTIVATION LINK",
+        html: `
+                  <h1>Please use the following link to activate your account</h1>
+                  <p>${client_url}/auth/activate/${token}</p>
+                  <hr />
+                  <p>This email may contain sensitive information</p>
+                  <p>${client_url}</p>
+              `,
+    };
+
+    sendmail(req, res, emailData)
+}
+
+const accountActivation = (req: Request, res: Response) => {
+    const { token } = req.body
+
+    const verifyCB: jwt.VerifyCallback = function (err: jwt.VerifyErrors|null, decoded: any) {
+        if (err) {
+            console.log('JWT VERIFY IN ACCOUNT ACTIVATION ERROR', err);
+            return res.status(401).json({
+                error: 'Expired link. Signup again'
+            });
+        }
+
+        const credentials = jwt.decode(token) as Credentials;
+        const user = new User(credentials);
+
+        user.save((err, user) => {
+            if (err) {
+                console.log('SAVE USER IN ACCOUNT ACTIVATION ERROR', err);
+                return res.status(401).json({
+                    error: 'Error saving user in database. Try signup again'
+                });
+            }
+            return res.json({
+                message: 'Signup success. Please signin.'
+            });
+        });
+    }
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION!, verifyCB)
+    } else {
+        return res.json({
+            message: 'Something went wrong. Try again.'
+        });
+    }
+}
+
+export { signUp, accountActivation }
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,10 +1,10 @@
 import { Router } from "express"
-import authController from "../controllers/auth"
+import { signUp, accountActivation } from "../controllers/auth"
 import userSignupValidator from "../validators/auth"
 import runValidation from "../validators"
 
 const router = Router()
 
-router.post('/api/signup', userSignupValidator, runValidation, authController)
-
+router.post('/api/signup', userSignupValidator, runValidation, signUp)
+router.post('/api/account-activation', accountActivation)
 export default router
