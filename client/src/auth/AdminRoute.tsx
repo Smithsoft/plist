d0272c2359bcca6671da256d594ff4cec896c890
Sarah Smith <sarah@smithsoft.com.au>
2021-05-05 09:27:03 +1000

    Add doc

---
+++ client/src/auth/AdminRoute.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from 'react'
+import { Route, Redirect, RouteProps } from 'react-router-dom'
+import { User } from '../types/User'
+import { isAuth } from './helpers'
+
+type PropType = {
+    component: FC
+} & RouteProps
+
+/**
+ * Component that wraps another component to produce a BrowserRouter
+ * component which only renders if the user is logged in as an admin
+ * user. If not logged in, or only logged in as a non-admin user, then
+ * the browser is redirected to the signin component.
+ * @param param0 Component and Route parameters as props.
+ * @returns Route wrapping the given component
+ */
+const AdminRoute: React.FC<PropType> = ({ component, ...rest }) => (
+    <Route
+        {...rest}
+        render={(props) =>
+            isAuth() && (isAuth() as User).role === 'admin' ? (
+                component(props)
+            ) : (
+                <Redirect
+                    to={{
+                        pathname: '/signin',
+                        state: { from: props.location }
+                    }}
+                />
+            )
+        }
+    ></Route>
+)
+
+export default AdminRoute
---
+++ client/src/core/Admin.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Admin: React.FC = () => (
+    <Layout>
+        <h1>Admin page</h1>
+    </Layout>
+)
+
+export default Admin
