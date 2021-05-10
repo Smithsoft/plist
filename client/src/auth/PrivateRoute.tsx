d5d60a536f73ecfd7978a10c4dcc5961f41d16a9
Sarah Smith <sarah@smithsoft.com.au>
2021-05-10 16:19:40 +1000

    Get user data into private route.
    This is now a profile screen.

--- client/src/auth/PrivateRoute.tsx
+++ client/src/auth/PrivateRoute.tsx
@@ -1,9 +1,9 @@
-import React, { FC } from 'react'
-import { Route, Redirect, RouteProps } from 'react-router-dom'
+import React from 'react'
+import { Route, Redirect, RouteProps, RouteComponentProps } from 'react-router-dom'
 import { isAuth } from './helpers'
 
 type PropType = {
-    component: FC
+    component: React.ComponentType<RouteComponentProps>
 } & RouteProps
 
 /**
@@ -13,22 +13,24 @@ type PropType = {
  * @param param0 Component and Route parameters as props.
  * @returns Route wrapping the given component
  */
-const PrivateRoute: React.FC<PropType> = ({ component, ...rest }) => (
-    <Route
-        {...rest}
-        render={(props) =>
-            isAuth() ? (
-                component(props)
-            ) : (
-                <Redirect
-                    to={{
-                        pathname: '/signin',
-                        state: { from: props.location }
-                    }}
-                />
-            )
-        }
-    ></Route>
-)
+const PrivateRoute: React.FC<PropType> = ({ component, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={(props) =>
+                isAuth() ? (
+                    React.createElement(component, props)
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: '/signin',
+                            state: { from: props.location }
+                        }}
+                    />
+                )
+            }
+        ></Route>
+    )
+}
 
 export default PrivateRoute
--- client/src/core/Private.tsx
+++ client/src/core/Private.tsx
@@ -1,10 +1,151 @@
 import React from 'react'
-import Layout from './Layout'
+import Layout from '../core/Layout'
+import axios, { AxiosRequestConfig } from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
 
-const Private: React.FC = () => (
-    <Layout>
-        <h1 style={{ fontFamily: 'Montserrat' }}>Private page</h1>
-    </Layout>
-)
+import { Redirect, RouteComponentProps } from 'react-router-dom'
+import { getCookie, isAuth, signout } from '../auth/helpers'
+import { User } from '../types/User'
+
+type StateType = {
+    role: string
+    name: string
+    email: string
+    password: string
+    buttonText: string
+}
+
+type ChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => void
+
+class Private extends React.Component<RouteComponentProps, StateType> {
+    state = {
+        role: '',
+        name: '',
+        email: '',
+        password: '',
+        buttonText: 'Submit'
+    }
+
+    componentDidMount(): void {
+        this.loadProfile()
+    }
+
+    loadProfile(): void {
+        const login = isAuth()
+        const user = login as User
+        const token = getCookie('token')
+        const conf: AxiosRequestConfig = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        axios
+            .get<User>(`${process.env.REACT_APP_API}/user/${user._id}`, conf)
+            .then((response) => {
+                console.log('PROFILE UPDATE', response)
+                const { role, name, email } = response.data
+                this.setState({ ...this.state, role, name, email })
+            })
+            .catch((error) => {
+                console.log('PRIVATE PROFILE UPDATE ERROR', error.response.data.console.error)
+                if (error.response.status == 401) {
+                    signout(() => {
+                        this.props.history.push('/')
+                    })
+                }
+            })
+    }
+
+    handleChange(name: string): ChangeHandler {
+        return (event: React.ChangeEvent<HTMLInputElement>) => {
+            console.log(`Handling: ${name} - ${event}`)
+            this.setState({ ...this.state, [name]: event.target.value })
+        }
+    }
+
+    clickSubmit(event: React.MouseEvent<HTMLButtonElement>): void {
+        event.preventDefault()
+        const { name, email, password } = this.state
+        this.setState({ ...this.state, buttonText: 'Submitting...' })
+        axios({
+            method: 'POST',
+            url: `${process.env.REACT_APP_API}/signup`,
+            data: { name, email, password }
+        })
+            .then((response) => {
+                console.log('SIGNUP SUCCESS')
+                this.setState({ ...this.state, name: '', email: '', password: '', buttonText: 'Submitted' })
+                toast.success(response.data.message)
+            })
+            .catch((error) => {
+                console.log('SIGNUP ERROR', error.response.data)
+                this.setState({ ...this.state, buttonText: 'Submit' })
+                toast.error(error.response.data.error)
+            })
+    }
+
+    updateForm(): JSX.Element {
+        const { role, name, email, password, buttonText } = this.state
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="updateFormRole" className="text-muted">
+                        Role
+                    </label>
+                    <input readOnly type="text" className="form-control" value={role} id="updateFormRole" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="updateFormName" className="text-muted">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        onChange={this.handleChange('name')}
+                        value={name}
+                        id="updateFormName"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="updateFormEmail" className="text-muted">
+                        Email
+                    </label>
+                    <input readOnly type="email" className="form-control" value={email} id="updateFormEmail" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="updateFormPassword" className="text-muted">
+                        Password
+                    </label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        onChange={this.handleChange('password')}
+                        value={password}
+                        id="updateFormPassword"
+                    />
+                </div>
+                <div>
+                    <button className="btn btn-primary" onClick={(e) => this.clickSubmit(e)}>
+                        {buttonText}
+                    </button>
+                </div>
+            </form>
+        )
+    }
+
+    render(): JSX.Element {
+        return (
+            <Layout>
+                <div className="col-md-6 offset-md-3">
+                    <ToastContainer />
+                    {!isAuth() ? <Redirect to="/" /> : null}
+                    <h1 className="pt-5 text-center">Private</h1>
+                    <p className="lead text-center">Profile update</p>
+                    {this.updateForm()}
+                </div>
+            </Layout>
+        )
+    }
+}
 
 export default Private
